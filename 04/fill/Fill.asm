fe7e8ebcc18e78960c7e59c748ef88203fa5292b
Ali A. AL-Darraji <sagads80@gmail.com>
2021-07-13 12:21:36 +0300

    solved project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,85 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//int Size = 256 * 512 = 8000 * 16
+
+//Got the size of screen and store it at Size
+@8000
+D = A
+@Size
+M = D 
+
+@Bcount
+M = 0
+@Wcount
+M = 0
+
+(START)
+//--------------------- START -----------------------------//
+//if keyboard is pressed, 
+//start printing white pixels
+@KBD
+D = M
+@PRINT_WHITE
+D; JNE
+
+//--------------------- Printing Black -----------------------------//
+(PRINT_BLACK)
+//Reset the Wcount to 0
+@Wcount
+M = 0
+
+//if I am at the last pixel, do nothing
+@Size
+D = M
+@Bcount
+D = D - M
+@START
+D;JEQ
+
+//put a black pixel to the screen, and increment the counter by one
+@SCREEN
+D = A
+@Bcount 
+A = M + D
+//screen address + Bcount address is selected
+
+M = 0
+
+@Bcount
+M = M + 1
+
+@START
+0;JMP
+
+
+//--------------------- Printing White -----------------------------//
+(PRINT_WHITE)
+
+//if I am at the last pixel, do nothing
+@Size
+D = M
+@Wcount
+D = D - M
+@START
+D;JEQ
+
+//Reset the Bcount to 0
+@Bcount
+M = 0
+
+//put a black pixel to the screen, and increment the counter by one
+@SCREEN
+D = A
+@Wcount 
+A = M + D
+//screen address + Bcount address is selected
+
+M = -1
+
+@Wcount
+M = M + 1
+
+@START
+0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,42 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+//setting our counter to 0
+@counter
+M = 0
+@sum
+M = 0
+
+
+//getting the first multiplicable value and repeat that
+
+
+(LOOP)
+@R0
+D = M
+@counter 
+D = D - M
+@STOP
+D; JEQ
+
+@counter
+M = M + 1
+
+@R1
+D = M
+@sum
+M = M + D
+@LOOP
+0; JMP
+
+(STOP)
+@sum
+D = M
+@R2 
+M = D
+
+(END)
+@END
+0;JMP
